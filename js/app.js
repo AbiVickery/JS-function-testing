48cac2ca0a90623c9cf83dd971016afaac063658
Abi Vickery <vickeryabi@gmail.com>
2020-06-01 14:37:17 -0500

    outside of grasshopper function

--- js/app.js
+++ js/app.js
@@ -1,1 +1,11 @@
-drawBox(green);
+// drawBox(green); 
+// drawBox() is a custom Grasshopper function and will not work in vanilla JS
+
+function drawBoxes(aString) {
+    let colorObject = {a : 'amber', b : 'blue', c : 'cyan', ...}
+    for (let element of aString) {
+      drawBox(colorObject[element]);
+    }
+  }
+
+  // replication of the drawbox function outside of grasshopper
